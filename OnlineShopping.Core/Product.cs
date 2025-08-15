3b99432117c9798cb40048901444963b6631f174
sargamsubedi <saragamsubedi998@gmail.com>
2025-08-15 10:09:54 +0545

    added api and library

---
+++ OnlineShopping.Core/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShopping.Core
+{
+    public class Product
+    {
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Price { get; set; }
+        public string? ProductPhoto { get; set; }
+        public string Description { get; set; }
+        public int CategoryId { get; set; }
+
+        // Navigation property 
+        public virtual Category? Category { get; set; }
+    }
+}
---
+++ OnlineShopping.WebAPI/Data/OnlineShoppingWebAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using OnlineShopping.Core;
+
+namespace OnlineShopping.WebAPI.Data
+{
+    public class OnlineShoppingWebAPIContext : DbContext
+    {
+        public OnlineShoppingWebAPIContext (DbContextOptions<OnlineShoppingWebAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<OnlineShopping.Core.Category> Category { get; set; } = default!;
+    }
+}
